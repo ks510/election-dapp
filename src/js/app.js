42a350dc270e367f1d21aa582e41fd9e5d2aebbd
Karen Suen <karen@Karens-MacBook-Air.local>
2019-03-12 18:55:35 +0000

    added app.js and comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,64 +1,94 @@
-App = {
+App = { //client app is an object initialized whenever the client loads the window
   web3Provider: null,
   contracts: {},
+  account: '0x0',
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
-    return await App.initWeb3();
+  // initialise the app and web3
+  init: function() {
+    return App.initWeb3();
   },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
+  // initialise web3 to connect client app to our local blockchain
+  // then call initialise contract
+  initWeb3: function() {
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask then use this for app
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
 
+  // initialize election contract then render app content on the page
+  // loads contract into frontend app for interaction
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    // load JSON file of contract artifact and use it to generate truffle contract
+    // getJSON works because we are using browser sync package (bs-config.json)
+    $.getJSON("Election.json", function(election) {
+      // Instantiate a new truffle contract from the artifact
+      // this is the object the app can interact with contracts through
+      App.contracts.Election = TruffleContract(election);
+      // Connect provider to interact with contract
+      App.contracts.Election.setProvider(App.web3Provider);
 
-    return App.bindEvents();
+      return App.render();
+    });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+  // display page
+  render: function() {
+    // declare election instance variable
+    var electionInstance;
+    var loader = $("#loader"); // gets control of these div ids from index.html
+    var content = $("#content");
+    // this is useful because we make asynchronous calls to get account and contract attributes
+    loader.show();
+    content.hide();
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+    // Load this client's account data and display
+    // eth.getCoinbase provides account the client is using to connect to blockchain
+    web3.eth.getCoinbase(function(err, account) {
+      // if there were no errors, then use this account for this app and show address
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+    // Load contract data and list all candidates on the page
+    // this code is similar to the JS client-side tests!
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance; // get contract instance
+      return electionInstance.candidatesCount(); // use count to ensure correct number of candidates are fetched in the loop
+    }).then(function(candidatesCount) {
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
 
-    var petId = parseInt($(event.target).data('id'));
+      // iterate through candidates mapping to get each candidates' values
+      for (var i = 1; i <= candidatesCount; i++) {
+        // call back function constructs a table row for render output
+        electionInstance.candidates(i).then(function(candidate) {
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
 
-    /*
-     * Replace me...
-     */
+          // Render candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+        });
+      }
+      // all asynchronous calls are done, loading finish and content ready to display
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
   }
-
 };
 
 $(function() {
