eef422ae84710f4f2418d970a8c156604b2504dc
Karen Suen <karen@Karens-MacBook-Air.local>
2019-03-13 00:51:02 +0000

    added tests for invalid candidate and double voting. updated index.html

--- test/election.js
+++ test/election.js
@@ -31,7 +31,7 @@ contract("Election", function (accounts) {
     return Election.deployed().then(function(instance) {
       electionInstance = instance;
       candidateId = 1; // candidate id to vote for
-      return electionInstance.vote(1, {from: accounts[0]}); // cast vote with first ganache account
+      return electionInstance.vote(candidateId, {from: accounts[0]}); // cast vote with first ganache account
     }).then(function(receipt) {
       return electionInstance.voters(accounts[0]);
     }).then(function(voted) {
@@ -40,7 +40,52 @@ contract("Election", function (accounts) {
     }).then(function(candidate) {
       var voteCount = candidate[2]; // get voteCount attribute
       assert(voteCount, 1, "candidate's vote count was incremented"); // check vote count was increased to 1
-    })
-  })
+    });
+  });
+
+  it("throws an exception for invalid candidate", function() {
+    return Election.deployed().then(function(instance) { // get contract instance
+      electionInstance = instance;
+      //vote for invalid candidate 0
+      return electionInstance.vote(0, { from: accounts[1] });
+    }).then(assert.fail).catch(function(error) { // assert exception thrown
+      // catch error and inject into callback function
+      // callback checks if the correct error message was received (revert)
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return electionInstance.candidates(1);
+    }).then(function(candidate1) { // check candidates votes did not increase to ensure state of contract is unchanged
+      var voteCount = candidate1[2];
+      assert(voteCount, 1, "candidate 1 did not receive any votes"); // previous test voted for this candidate
+      return electionInstance.candidates(2);
+    }).then(function(candidate2) {
+      var voteCount = candidate2[2];
+      assert(voteCount, 0, "candidate 2 did not receive any votes"); // no votes for this candidate yet
+    });
+  });
+
+  it("throws an exception for double voting", function() {
+    return Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      candidateId = 2; // vote for candidate 2 using account 1
+      electionInstance.vote(2, { from: accounts[1]} );
+      return electionInstance.candidates(2);
+    }).then(function(candidate) { // check vote count incremented
+      var voteCount = candidate[2];
+      assert(voteCount, 1, "accepts first vote");
+      // attempt to vote again
+      return electionInstance.vote(candidateId, { from: accounts[1] });
+    }).then(assert.fail).catch(function(error) {
+      // check error was thrown, catch error and check error is expected one
+      assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+      return electionInstance.candidates(1);
+    }).then(function(candidate1) { // check vote counts did not increase for any candidates
+      var voteCount = candidate1[2];
+      assert(voteCount, 1, "candidate 1 vote count unchanged");
+      return electionInstance.candidates(2);
+    }).then(function(candidate2) {
+      var voteCount = candidate2[2];
+      assert(voteCount, 1, "candidate 2 vote count unchanged");
+    });
+  });
 
 });
